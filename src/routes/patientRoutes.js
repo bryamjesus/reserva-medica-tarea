a70d39d14733e43e92517c6967f86678cbe7cb95
Bryam Jesus Talledo Garcia <btalledog@odybank.com.pe>
2022-11-01 19:24:40 -0500

    Update routes

--- src/routes/patientRoutes.js
+++ src/routes/patientRoutes.js
@@ -2,23 +2,23 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/patientController')
 
-router.get('/', (req, res) => {
+router.get('/listar', (req, res) => {
   controller.listAllPatient(req, res)
 })
 
-router.get('/:id', (req, res) => {
+router.get('/mostrar/:id', (req, res) => {
   controller.listOnePatient(req, res)
 })
 
-router.post('/', (req, res) => {
+router.post('/crear', (req, res) => {
   controller.createPatient(req, res)
 })
 
-router.put('/:id', (req, res) => {
+router.put('/actualizar/:id', (req, res) => {
   controller.updatePatient(req, res)
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/eliminar/:id', (req, res) => {
   controller.deletePatient(req, res)
 })
 
